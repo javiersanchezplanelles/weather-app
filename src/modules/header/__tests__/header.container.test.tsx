d25844f260f8184cb9d1f96efd202ad5b8f5795a
Javier <25299226+javiersanchezplanelles@users.noreply.github.com>
2023-07-04 11:11:25 +0200

    feat: solve test

--- src/modules/header/__tests__/header.container.test.tsx
+++ src/modules/header/__tests__/header.container.test.tsx
@@ -2,8 +2,10 @@ import { render, screen } from "@testing-library/react"
 import { HeaderContainer } from "../header.container"
 
 describe("Header container", () => {
+  const onHandleClick = jest.fn()
+
   it("should render the header", () => {
-    render(<HeaderContainer />)
+    render(<HeaderContainer onHandleClick={onHandleClick} />)
     const englishButton = screen.getByRole("button", { name: /english/i })
     const spanishButton = screen.getByRole("button", { name: /spanish/i })
 
