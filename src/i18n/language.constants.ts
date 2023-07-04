81b167282f41b3abdfd8f722db91ab23472b9119
Javier <25299226+javiersanchezplanelles@users.noreply.github.com>
2023-07-04 09:04:30 +0200

    feat: add i18n

---
+++ src/i18n/language.constants.ts
@@ -0,0 +1,17 @@
+enum LANGUAGES {
+  ENGLISH = "English",
+  SPANISH = "Spanish",
+}
+
+export const LanguageList = [
+  {
+    name: LANGUAGES.ENGLISH,
+    code: "en",
+    country: "UK",
+  },
+  {
+    name: LANGUAGES.SPANISH,
+    code: "es",
+    country: "ES",
+  },
+]
